0ba7de6486f4ecf8f360c9d86e501c1705acd2d2
Kyomen <kenkakyomen@hotmail.com>
2020-09-30 15:34:30 -0300

    adição da classe Tabuleiro que eu tinha esquecido e implementação parcial dela, com a função que inicializa o tabuleiro

---
+++ Xadrez/src/Tabuleiro.java
@@ -0,0 +1,26 @@
+public class Tabuleiro {
+    private final Posicao[][] posicoes = new Posicao[8][8];
+
+    public Tabuleiro() {
+        inicializaTabuleiro();
+    }
+
+    public Posicao[][] getPosicoes() {
+        return posicoes;
+    }
+
+    private void inicializaTabuleiro() {
+        char coluna;
+        int linha;
+        for (int i = 0; i < 8; i++) {
+            linha = 7 - i;
+            for (int j = 0; j < 8; j++) {
+                coluna = (char) (j + 'a'); //usando ASCII para saber a letra da coluna
+                this.getPosicoes()[i][j] = new Posicao(linha, coluna);
+                if (i == 0 || i == 1 || i == 6 || i == 7) {
+                    this.getPosicoes()[i][j].setOcupada(true);
+                }
+            }
+        }
+    }
+}
