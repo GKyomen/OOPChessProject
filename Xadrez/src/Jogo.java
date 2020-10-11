ffd90d76e822dc82ff4fbc7e683838fe22cb7b6a
GKyomen <kenkakyomen@hotmail.com>
2020-10-11 12:00:15 -0300

    Implementada a parte possível da classe Jogo (não depende da classe Peça)

--- Xadrez/src/Jogo.java
+++ Xadrez/src/Jogo.java
@@ -1,3 +1,11 @@
 public class Jogo {
-    
+    private int estadoDoJogo;
+    private Tabuleiro tabuleiro;
+    private boolean ehVezDoJogador1;
+
+    public Jogo() {
+        this.estadoDoJogo = 0;
+        this.tabuleiro = new Tabuleiro();
+        this.ehVezDoJogador1 = true;
+    }
 }
